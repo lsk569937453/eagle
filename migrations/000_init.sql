fc93dc9039dfca4ccce95faf8cba21ff14415934
shikai liu <lsk569937453@gmail.com>
2024-05-08 16:16:14 +0800

    Some fix of clippy problems.

--- migrations/000_init.sql
+++ migrations/000_init.sql
@@ -1,11 +1,14 @@
-CREATE TABLE IF NOT EXISTS system (
+CREATE TABLE IF NOT EXISTS system_data (
     id INTEGER PRIMARY KEY,
-    total_memory INTEGER NOT NULL,
-    used_memory INTEGER NOT NULL,
-    total_swap INTEGER NOT NULL,
-    used_swap INTEGER NOT NULL,
-    cpu_usage TEXT NOT NULL,
-    network_usage INTEGER NOT NULL DEFAULT 0,
+    total_memory INTEGER NOT NULL DEFAULT 0,
+    used_memory INTEGER NOT NULL DEFAULT 0,
+    total_swap INTEGER NOT NULL DEFAULT 0,
+    used_swap INTEGER NOT NULL DEFAULT 0,
+    cpu_usage FLOAT NOT NULL,
+    disk_usage_read_bytes INTEGER NOT NULL DEFAULT 0,
+    disk_usage_written_bytes INTEGER NOT NULL DEFAULT 0,
+     network_usage_upload_bytes INTEGER NOT NULL DEFAULT 0,
+      network_usage_download_bytes INTEGER NOT NULL DEFAULT 0,
     timestamp NOT NULL DEFAULT (
         strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime')
     )
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use clap::Parser;
 #[macro_use]
 extern crate anyhow;
 use sysinfo::{Pid, System};
-use tokio::time::{sleep, Interval};
+use tokio::time::{sleep, Instant, Interval};
 mod base_data;
 use crate::database::init::create_pool;
 use base_data::process_data::ProcessData;
@@ -32,24 +32,31 @@ pub async fn start() -> Result<(), anyhow::Error> {
     let pool = create_pool().await?;
     let mut sys = System::new_all();
     let mut interval = interval(Duration::from_millis(1000));
+    let mut networks = Networks::new_with_refreshed_list();
+
     loop {
         interval.tick().await;
         sys.refresh_all();
         println!("=> system:");
         // RAM and swap information:
-        println!("total memory: {} bytes", sys.total_memory());
-        println!("used memory : {} bytes", sys.used_memory());
-        println!("total swap  : {} bytes", sys.total_swap());
-        println!("used swap   : {} bytes", sys.used_swap());
+        let total_memory = sys.total_memory();
+        let used_memory = sys.used_memory();
+        let total_swap = sys.total_swap();
+        let used_swap = sys.used_swap();
 
+        let mut cpu_uage = 0.0;
+        let count = sys.cpus().len();
         for cpu in sys.cpus() {
-            print!("{}% ", cpu.cpu_usage());
+            cpu_uage += cpu.cpu_usage();
         }
+        cpu_uage /= (count * 100) as f32;
+        println!("cpu usage:{}", cpu_uage);
         let mut read_bytes = 0;
         let mut total_read_bytes = 0;
 
         let mut write_bytes = 0;
         let mut total_write_bytes = 0;
+
         for (pid, process) in sys.processes() {
             let disk_usage = process.disk_usage();
             read_bytes += disk_usage.read_bytes;
@@ -63,17 +70,34 @@ pub async fn start() -> Result<(), anyhow::Error> {
         );
 
         // Network interfaces name, total data received and total data transmitted:
-        let networks = Networks::new_with_refreshed_list();
-        println!("=> networks:");
-        for (interface_name, data) in &networks {
-            println!(
-                "{interface_name}: {} B (down) / {} B (up)",
-                data.total_received(),
-                data.total_transmitted(),
-            );
-            // If you want the amount of data received/transmitted since last call
-            // to `Networks::refresh`, use `received`/`transmitted`.
+        let mut total_received = 0;
+        let mut total_upload = 0;
+        networks.refresh();
+        for (_, data) in &networks {
+            total_received += data.received();
+            total_upload += data.transmitted();
         }
+        println!("network usage:{},{}", total_received, total_upload);
+        let sql = format!(
+            r#"INSERT INTO system_data (total_memory, used_memory, total_swap, used_swap,cpu_usage,disk_usage_read_bytes,disk_usage_written_bytes,network_usage_upload_bytes,network_usage_download_bytes) 
+        VALUES ("{}","{}","{}","{}","{}","{}","{}","{}","{}")"#,
+            total_memory,
+            used_memory,
+            total_swap,
+            used_swap,
+            cpu_uage,
+            read_bytes,
+            write_bytes,
+            total_upload,
+            total_received
+        );
+        println!("sql is {}", sql);
+        let now = Instant::now();
+        sqlx::query(&sql)
+            .execute(&pool)
+            .await
+            .map_err(|e| anyhow!("insert error, the error is {}", e))?;
+        println!("time elasmpsed:{}ms", now.elapsed().as_millis());
     }
 
     Ok(())
